73ea7631f213dc7c25ae99d69c59330c951ae734
ahmadml <57864630+ahmadml@users.noreply.github.com>
2024-01-29 22:28:19 +0200

    Update play.java

--- play.java
+++ play.java
@@ -6,28 +6,4 @@ public class play {
 	public play(Node n) {
 		this.start = n;
 	}
-	
-//	public ArrayList<Node> findson() {
-//		ArrayList<Node> l = new ArrayList<Node>();
-//		String [][] root = this.start.getState();
-//		for (int i = 0; i < root.length; i++) {
-//			for (int j = 0; j < root.length; j++) {
-//				if(i==0 && j==0) {
-//					if(root[i][j+1].equals("_")) {
-//						System.out.println(1);
-//						Node son = this.start.move_right(root, i, j, root[i][j]);
-//						l.add(son);
-//					}
-//					if(root[i][j+1].equals("_")) {
-//						System.out.println(2);
-//						Node son = this.start.move_down(root, i, j, root[i][j]);
-//						l.add(son);
-//					}
-//				}
-//			}
-//		}
-//		return l;
-//	}
-	
-	
 }
